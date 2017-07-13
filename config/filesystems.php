eeab09b200b96eb0ab974fbf23caadfd20679a4b
miaomin <miaomin@bitmap3d.com.cn>
2017-07-13 17:15:38 +0800

    add laravel-admin function

--- config/filesystems.php
+++ config/filesystems.php
@@ -43,6 +43,12 @@ return [
 
     'disks' => [
 
+        'admin' => [
+            'driver' => 'local',
+            'root' => public_path('upload'),
+            'visibility' => 'public',
+        ],
+
         'local' => [
             'driver' => 'local',
             'root' => storage_path('app'),
@@ -51,7 +57,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
         ],
 
